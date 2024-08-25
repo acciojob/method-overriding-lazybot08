2e9984b1bc17e359c07def7b02c33295e22f7cf7
Aryan Jaiswal <aryanj868@gmai.com>
2024-08-25 09:36:31 +0530

    adfaaf

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,24 @@
 package com.driver;
 
-class A {
-	String meth() {
-		return "Invoking method from class A";
-	}
-}
-
-class B extends A {
-
-	@Override
-	String meth() {
-		// TODO Auto-generated method stub
-		return "Method is overridden in Extended class B";
-	}
-	
-}
 
 public class Main {
-	public static void main(String[] args) {
-//		B obj1 = new B();
-//		System.out.println(obj1.meth());
-//		Invoking method from class A
-		
-		B obj2 = new B();
-		System.out.println(obj2.meth());
-//		Method is overridden in Extended class B
-	}
+
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        B objB = new B();
+        objB.meth();
+    }
 }
